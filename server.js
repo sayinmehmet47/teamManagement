3ff286a2ebedeeb828ffb9c86617a0ab8fc485cd
unknown <sayinmehmet47@gmail.com>
2021-12-03 22:28:56 +0200

    removed cors options

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use(express.json());
 
 require("dotenv").config();
 mongoose.connect(process.env.MONGO_DB).then(console.log("connected"));
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/items", items);
 app.use("/api/users", users);
