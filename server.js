a18671ea3ef8afc478b9fcec9b6364f32fdc444b
unknown <sayinmehmet47@gmail.com>
2021-12-04 11:22:54 +0200

    added secret

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 require("dotenv").config();
 mongoose.connect(process.env.MONGO_DB).then(console.log("connected"));
 
-app.use(cors());
+app.use(cors({ origin: new URL("http://localhost:5000"), credentials: true })); // Add this 'new' keyword to URL
 
 app.use("/api/items", items);
 app.use("/api/users", users);
@@ -21,18 +21,6 @@ app.use(express.static("client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
-
-// if (
-//   process.env.NODE_ENV === "production" ||
-//   process.env.NODE_ENV === "staging"
-// ) {
-//   // Set static folder
-//   app.use(express.static("client/build"));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
 
 const listener = app.listen(process.env.PORT || 5000, () => {
   console.log("Your app is listening on port " + listener.address().port);
