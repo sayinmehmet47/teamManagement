2551251fe68ac794a58e21c6e0ebd7a5d0828ddf
mehmet <sayinmehmet47@mehmets-MacBook-Air.local>
2022-05-08 12:38:30 +0200

    new refs added to schemas

--- models/Items.js
+++ models/Items.js
@@ -1,15 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
   name: {
     type: String,
-    required: "name cannot be blank",
+    required: 'name cannot be blank',
   },
   date: {
     type: Date,
     default: Date.now(),
   },
   players: [{ name: String, age: Number }],
+
+  team: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  owner: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
 });
 
-module.exports = Item = mongoose.model("item", schema);
+module.exports = Item = mongoose.model('item', schema);
