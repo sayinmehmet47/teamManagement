f3c233c6da5016f9fd9cd8e7e003341e6b1c2976
mehmet <sayinmehmet47@mehmets-MacBook-Air.local>
2022-05-10 12:00:33 +0200

    server and app file seperated

--- server.js
+++ server.js
@@ -1,35 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const app = express();
-
-const routes = require('./routes/api/routes');
+const app = require('./app');
+require('dotenv').config();
 var server = require('http').Server(app);
 const io = require('./socket/socket').init(server);
-app.use(express.json());
-
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_DB).then(console.log('connected'));
-const corsOptions = {
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-
-app.use(routes);
-
-if (
-  process.env.NODE_ENV === 'production' ||
-  process.env.NODE_ENV === 'staging'
-) {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 io.on('connection', (socket) => {
   console.log('New client connected');
