8191347ec8158d3620d8fe2ebe81b090da8071f5
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-13 14:15:37 +0300

    added helmet and compression

--- server/app.js
+++ server/app.js
@@ -1,32 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const helmet = require("helmet");
+const compression = require("compression");
 
-const routes = require('./routes/api/routes');
+const routes = require("./routes/api/routes");
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_DB).then(console.log('connected'));
+mongoose.connect(process.env.MONGO_DB).then(console.log("connected"));
 
 const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 
+app.use(helmet()); // Setting headers covered by helmet secure
+app.use(compression()); // set assets compressed by compression
+
 app.use(cors(corsOptions));
 
 app.use(routes);
 
 if (
-  process.env.NODE_ENV === 'production' ||
-  process.env.NODE_ENV === 'staging'
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
 ) {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
