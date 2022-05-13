e579fa53a866a8c25f9cd759ee2583cac2dc66a1
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-13 18:55:22 +0300

    added helmet

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const helmet = require("helmet");
 
-const routes = require('./routes/api/routes');
+const routes = require("./routes/api/routes");
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_DB).then(console.log('connected'));
+mongoose.connect(process.env.MONGO_DB).then(console.log("connected"));
 
 const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
@@ -15,18 +16,19 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(helmet());
 
 app.use(routes);
 
 if (
-  process.env.NODE_ENV === 'production' ||
-  process.env.NODE_ENV === 'staging'
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
 ) {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
