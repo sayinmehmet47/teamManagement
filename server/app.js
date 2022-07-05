40e30675bf98f396f14fb91b794b328d1a5ec616
mehmet <sayinmehmet47@gmail.com>
2022-07-05 21:33:11 +0200

    Initial Commit. Added heroku.yml.

--- server/app.js
+++ server/app.js
@@ -8,7 +8,8 @@ const path = require('path');
 const routes = require('./routes/api/routes');
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 mongoose.connect(process.env.MONGO_DB).then(console.log('connected'));
 
 const corsOptions = {
