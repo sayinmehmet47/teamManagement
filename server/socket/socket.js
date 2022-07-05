c2d23612142f6259e9eb1623c38a3bfefbb531d0
mehmet <sayinmehmet47@gmail.com>
2022-07-05 21:55:30 +0200

    Initial Commit. Added heroku.yml.

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -6,6 +6,8 @@ module.exports = {
       cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
+        allowedHeaders: ['my-custom-header'],
+        credentials: true,
       },
     }));
   },
