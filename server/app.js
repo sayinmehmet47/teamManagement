95b83805faa43d50e09b2f6da0eabb712951e4d9
mehmet <sayinmehmet47@gmail.com>
2022-07-05 21:25:39 +0200

    Initial Commit. Added heroku.yml.

--- server/app.js
+++ server/app.js
@@ -28,10 +28,8 @@ if (
   process.env.NODE_ENV === 'staging'
 ) {
   // Set static folder
-  app.use(express.static('client/build'));
-
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
 
