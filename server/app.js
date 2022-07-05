55a0dfe0517acd73ee1e9c237591d70dc30f89eb
mehmet <sayinmehmet47@gmail.com>
2022-07-05 20:12:24 +0200

    Initial Commit. Added heroku.yml.

--- server/app.js
+++ server/app.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
-const helmet = require("helmet");
-const compression = require("compression");
-
-const routes = require("./routes/api/routes");
+const helmet = require('helmet');
+const compression = require('compression');
+const path = require('path');
+const routes = require('./routes/api/routes');
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_DB).then(console.log("connected"));
+mongoose.connect(process.env.MONGO_DB).then(console.log('connected'));
 
 const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
@@ -24,14 +24,14 @@ app.use(compression());
 app.use(routes);
 
 if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "staging"
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'staging'
 ) {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
