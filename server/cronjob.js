32028184a3aad6cd522778c9319d0e1ae5fc90ba
mehmet <sayinmehmet47@gmail.com>
2023-09-16 13:19:40 +0200

    feat: add cron job

---
+++ server/cronjob.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const cron = require('node-cron');
+
+const allBooksURL = 'https://kitapkurdu.onrender.com/api/books/allBooks?page=1';
+
+const myCronJob = () => {
+  // This function will run every 10 minutes
+  cron.schedule('*/10 * * * *', async () => {
+    try {
+      const response = await axios.get(allBooksURL);
+      console.log('cron job started');
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+};
+
+module.exports = { myCronJob };
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 require('dotenv').config();
 var server = require('http').Server(app);
 const io = require('./socket/socket').init(server);
-
+const myCronJob = require('./cronjob').myCronJob;
 io.on('connection', (socket) => {
   console.log('New client connected');
 
@@ -10,6 +10,8 @@ io.on('connection', (socket) => {
     console.log('Client disconnected');
   });
 });
+
+myCronJob();
 
 server.listen(process.env.PORT || 5000, () => {
   console.log('socket.io server started on port 5000');
