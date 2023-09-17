259f2f153834f81546c4f7bb2b762dbdf0569999
mehmet sayin <sayinmehmet47@gmail.com>
2023-09-17 12:51:57 +0200

    Update cronjob.js

--- server/cronjob.js
+++ server/cronjob.js
@@ -5,7 +5,7 @@ const allBooksURL = 'https://kitapkurdu.onrender.com/api/books/allBooks?page=1';
 
 const myCronJob = () => {
   // This function will run every 10 minutes
-  cron.schedule('*/10 * * * *', async () => {
+  cron.schedule('*/5 * * * *', async () => {
     try {
       const response = await axios.get(allBooksURL);
       console.log('cron job started');
